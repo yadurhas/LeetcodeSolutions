da7f3cce321e3830152d1d707a8597922335e93d
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-03-20 10:18:03 +0530

    Update 1669. Merge In Between Linked Lists.js
    Solution to the Daily Leetcode challenge dated 20/03/2024

--- 1669. Merge In Between Linked Lists/1669. Merge In Between Linked Lists.js
+++ 1669. Merge In Between Linked Lists/1669. Merge In Between Linked Lists.js
@@ -1,1 +1,36 @@
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {number} a
+ * @param {number} b
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeInBetween = function(list1, a, b, list2) {
+    let list2_head = list2, list2_tail, node;
+    node = list2_head;
+    while (node.next != null) {
+        node = node.next;
+    }
+    list2_tail = node;
+    node = list1;
+    let prev = null, ind = 0;
+    while (node != null) {
+        if (ind === a) {
+            prev.next = list2_head;
+        }
+        if (ind === b) {
+            list2_tail.next = node.next;
+        }
+        prev = node;
+        node = node.next;
+        ind++;
+    }
+    return list1;
+};
