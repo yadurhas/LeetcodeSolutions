cb4279016eae2d2c6efa0b24d03c4ab71f0b4d87
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-02 08:30:02 +0530

    Update 205. Isomorphic Strings.js
    previous solution was wrong

--- 205. Isomorphic Strings/205. Isomorphic Strings.js
+++ 205. Isomorphic Strings/205. Isomorphic Strings.js
@@ -4,21 +4,21 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-   let freq1 = new Array(26).fill(0);
-   let freq2 = new Array(26).fill(0);
-   function getFreq(str, freq) {
-    for (let i = 0; i < str.length; i++) {
-        freq[str.charCodeAt(i) - 97]++;
+    let map1 = new Map();
+    let set1 = new Set();
+    for (let i = 0; i < s.length; i++) {
+        if (!map1.has(s[i])) {
+            if (set1.has(t[i])) {
+                return false;
+            }
+            map1.set(s[i], t[i]);
+            set1.add(t[i]);
+        }
+        else {
+            if (map1.get(s[i]) != t[i]) {
+                return false;
+            }
+        }
     }
-   } 
-   getFreq(s, freq1);
-   getFreq(t, freq2);
-   freq1.sort((a, b) => a - b);
-   freq2.sort((a, b) => a - b);
-   for (let i = 0; i < 26; i++) {
-    if (freq1[i] != freq2[i]) {
-        return false;
-    }
-   }
-   return true;
+    return true;
 };
