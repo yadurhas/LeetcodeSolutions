096ea9922fdb714f85d088e263612e89a2038de3
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-04 11:08:58 +0530

    Update 1614. Maximum Nesting Depth of the Parentheses.js
    Added JS solution to Daily Leetcode Challenge dated 4/04/2024

--- 1614. Maximum Nesting Depth of the Parentheses/1614. Maximum Nesting Depth of the Parentheses.js
+++ 1614. Maximum Nesting Depth of the Parentheses/1614. Maximum Nesting Depth of the Parentheses.js
@@ -1,1 +1,19 @@
-
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let depth = 0, max_depth = 0;
+    for (let char of s) {
+        if (char === '(') {
+            depth++;
+            if (depth > max_depth) {
+                max_depth = depth;
+            }
+        }
+        else if (char === ')') {
+            depth--;
+        }
+    }
+    return max_depth;
+};
