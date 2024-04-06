231cdf2545a3df8950ed092b1d04e8a59a438de7
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-06 08:07:09 +0530

    Update 1249. Minimum Remove to Make Valid Parentheses.js
    Solution to daily leetcode challenge dated 6/04/2024

--- 1249. Minimum Remove to Make Valid Parentheses/1249. Minimum Remove to Make Valid Parentheses.js
+++ 1249. Minimum Remove to Make Valid Parentheses/1249. Minimum Remove to Make Valid Parentheses.js
@@ -1,1 +1,33 @@
-
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function(s) {
+    let stack = [];
+    let validParenthesis = [];
+    let size = s.length;
+    for (let i = 0; i < size; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        }
+        else if (s[i] === ')') {
+            if (stack.length != 0) {
+                validParenthesis.push(i);
+                validParenthesis.push(stack.pop());
+            }
+        }
+    }
+    validParenthesis.sort((a, b) => a - b);
+    let ind1 = 0;
+    let res = []
+    for (let i = 0; i < size; i++) {
+        if (s[i] === '(' || s[i] === ')') {
+            if (validParenthesis[ind1] !== i) {
+                continue;
+            }
+            ind1++;
+        }
+        res.push(s[i]);
+    }
+    return res.join('');
+};
