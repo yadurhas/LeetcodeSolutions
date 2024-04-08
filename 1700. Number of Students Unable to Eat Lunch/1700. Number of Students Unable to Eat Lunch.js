40ad1f9aabe90ddd90cd6cb6a84069b2715ca24b
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-08 10:24:38 +0530

    Update 1700. Number of Students Unable to Eat Lunch.js

--- 1700. Number of Students Unable to Eat Lunch/1700. Number of Students Unable to Eat Lunch.js
+++ 1700. Number of Students Unable to Eat Lunch/1700. Number of Students Unable to Eat Lunch.js
@@ -1,1 +1,33 @@
-
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+    let count1 = 0, count2 = 0, size = students.length;
+    for (let i  = 0; i < size; i++) {
+        count1 += students[i];
+        count2 += sandwiches[i];
+    }
+    if (count1 === count2) {
+        return 0;
+    }
+    if (count1 < count2) {
+        for (let i = 0; i < size; i++) {
+            if (sandwiches[i] === 1) {
+                count1--;
+                if (count1 === -1) {
+                    return size - i;
+                }
+            }
+        }
+    }
+    for (let i = 0; i < size; i++) {
+        if (sandwiches[i] === 0) {
+            count1++;
+            if (count1 === size + 1) {
+                return size - i;
+            }
+        }
+    }
+};
