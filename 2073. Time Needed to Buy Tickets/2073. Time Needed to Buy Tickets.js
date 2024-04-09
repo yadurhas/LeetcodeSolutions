4a9661618c6fb23f926b75c274bf0a4281a9b867
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-09 16:07:20 +0530

    Update 2073. Time Needed to Buy Tickets.js

--- 2073. Time Needed to Buy Tickets/2073. Time Needed to Buy Tickets.js
+++ 2073. Time Needed to Buy Tickets/2073. Time Needed to Buy Tickets.js
@@ -1,1 +1,17 @@
-
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+    let t = tickets[k], size = tickets.length;
+    let time = 0;
+    for (let i = 0; i <= k; i++) {
+        time += Math.min(tickets[i], t);
+    }
+    t = t - 1;
+    for (let i = k + 1; i < size; i++) {
+        time += Math.min(tickets[i], t);
+    }
+    return time;
+};
