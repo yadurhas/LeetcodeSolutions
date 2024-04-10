e9942129410413b2748861d84784e918db95472b
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-10 10:58:44 +0530

    Update 950. Reveal Cards In Increasing Order.js
    Solution to daily Leetcode Challenge dated 10/04/2024

--- 950. Reveal Cards In Increasing Order/950. Reveal Cards In Increasing Order.js
+++ 950. Reveal Cards In Increasing Order/950. Reveal Cards In Increasing Order.js
@@ -1,1 +1,36 @@
-
+/**
+ * @param {number[]} deck
+ * @return {number[]}
+ */
+var deckRevealedIncreasing = function(deck) {
+    let size = deck.length;
+    let index = new Array(size);
+    for (let i = 0; i < size; i++) {
+        index[i] = i;
+    }
+    let t = [];
+    let resIndex = [];
+    while (index.length !== 0) {
+        for (let i = 0; i < index.length; i = i + 2) {
+            resIndex.push(index[i]);
+            if (i + 1 < index.length) {
+                t.push(index[i + 1]);
+            }
+            else {
+                if (t.length !== 0) {
+                    t.push(t.shift());
+                }
+            }
+        }
+        index = t;
+        t = [];
+        //console.log(index, t);
+    }
+    let res = new Array(size);
+    deck.sort((a, b) => a - b);
+    for (let i = 0; i < size; i++) {
+        res[resIndex[i]] = deck[i]; 
+    }
+    //console.log(res);
+    return res;
+};
