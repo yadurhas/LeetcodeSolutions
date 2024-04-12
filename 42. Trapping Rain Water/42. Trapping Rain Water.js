ce3827b4af20fe07abc1c35c07b8e77ac0abf861
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-12 08:17:04 +0530

    Update 42. Trapping Rain Water.js
    Solution to Daily Leetcode Challenge dated 12/04/2024

--- 42. Trapping Rain Water/42. Trapping Rain Water.js
+++ 42. Trapping Rain Water/42. Trapping Rain Water.js
@@ -1,1 +1,31 @@
-
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let prev = height[0], temp = 0, trapped = 0, prevIndex = 0;
+    for (let i = 1; i < height.length; i++) {
+        if (height[i] >= prev) {
+            trapped += temp;
+            prev = height[i];
+            prevIndex = i;
+            temp = 0;
+        }
+        else {
+            temp += prev - height[i];
+        }
+    }
+    temp = 0;
+    prev = height[height.length - 1];
+    for (let i = height.length - 2; i >= prevIndex; i--) {
+        if (height[i] >= prev) {
+            trapped += temp;
+            prev = height[i];
+            temp = 0;
+        }
+        else {
+            temp += prev - height[i];
+        }
+    }
+    return trapped;
+};
