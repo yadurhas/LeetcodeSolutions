84d5891eecd3d4bd0eac5110da3c836644d59002
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-14 08:50:03 +0530

    Update 404. Sum of Left Leaves.js

--- 404. Sum of Left Leaves/404. Sum of Left Leaves.js
+++ 404. Sum of Left Leaves/404. Sum of Left Leaves.js
@@ -1,1 +1,29 @@
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    let sum = 0;
+    function traverse(node, flag) {
+        if (node !== null) {
+            if (node.left === null && node.right === null) {
+                if (flag === true) {
+                    sum += node.val;
+                }
+                return;
+            }
+            traverse(node.left, true);
+            traverse(node.right, false);
+        }
+    }
+    traverse(root);
+    return sum;
+};
