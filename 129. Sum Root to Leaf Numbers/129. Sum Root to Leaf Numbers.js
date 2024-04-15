ebad9fb3f46557a62a50c517de77e38fc911c353
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-15 14:26:21 +0530

    Update 129. Sum Root to Leaf Numbers.js
    Solution to Daily Leetcode Challenge dated 15/04/2024

--- 129. Sum Root to Leaf Numbers/129. Sum Root to Leaf Numbers.js
+++ 129. Sum Root to Leaf Numbers/129. Sum Root to Leaf Numbers.js
@@ -1,1 +1,30 @@
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+    let sum = 0;
+    function traverse(node, num) {
+        num = num * 10 + node.val;
+        if (node.left === null && node.right === null) {
+            sum += num;
+            return;
+        }
+        if (node.left !== null) {
+            traverse(node.left, num);
+        }
+        if (node.right !== null) {
+            traverse(node.right, num);
+        }
+    }
+    traverse(root, 0);
+    return sum;
+};
