26ec818464db91f6cf906db6c0d9a20bfa6064be
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-16 11:30:30 +0530

    Update 623. Add One Row to Tree.js
    Solution to Daily Leetcode Challenge dated 16/04/2024
    
    Took more time than required because of small confusion in treating functions as classes in JS and the need to use new keyword to get a new object.

--- 623. Add One Row to Tree/623. Add One Row to Tree.js
+++ 623. Add One Row to Tree/623. Add One Row to Tree.js
@@ -1,1 +1,35 @@
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @param {number} depth
+ * @return {TreeNode}
+ */
+var addOneRow = function(root, val, depth) {
+    if (depth === 1) {
+        return new TreeNode(val, root);
+    }
+    function traverse(node, val, depth) {
+        if (node !== null) {
+            if (depth === 2) {
+                let t1, t2;
+                t1 = new TreeNode(val, node.left);
+                t2 = new TreeNode(val, null, node.right);
+                node.left = t1;
+                node.right = t2;
+                return;
+            }
+            traverse(node.left, val, depth - 1);
+            traverse(node.right, val, depth - 1);
+        }
+    }
+    traverse(root, val, depth);
+    return root;
+};
