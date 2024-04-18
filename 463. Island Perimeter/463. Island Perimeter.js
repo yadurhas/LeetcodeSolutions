f0e2f4d1d1c768577eaabe7f4f36a1f842295664
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-18 11:40:30 +0530

    Update 463. Island Perimeter.js

--- 463. Island Perimeter/463. Island Perimeter.js
+++ 463. Island Perimeter/463. Island Perimeter.js
@@ -1,1 +1,18 @@
-
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function(grid) {
+    let perimeter = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) {
+                perimeter += (i - 1 >= 0) ? (grid[i - 1][j] === 0 ? 1: 0) : 1;
+                perimeter += (j - 1 >= 0) ? (grid[i][j - 1] === 0 ? 1: 0) : 1;
+                perimeter += (i + 1 < grid.length) ? (grid[i + 1][j] === 0 ? 1: 0) : 1;
+                perimeter += (j + 1 < grid[i].length) ? (grid[i][j + 1] === 0 ? 1: 0) : 1;   
+            }
+        }
+    }
+    return perimeter;
+};
