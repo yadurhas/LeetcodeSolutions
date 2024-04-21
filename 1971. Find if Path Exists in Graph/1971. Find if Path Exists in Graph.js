4999de6683f0593f3ea5c25203fb7dda790ce557
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-21 10:58:05 +0530

    Update 1971. Find if Path Exists in Graph.js

--- 1971. Find if Path Exists in Graph/1971. Find if Path Exists in Graph.js
+++ 1971. Find if Path Exists in Graph/1971. Find if Path Exists in Graph.js
@@ -1,1 +1,38 @@
-
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    if (source === destination) {
+        return true;
+    }
+    let adjList;
+    adjList = new Array(n);
+    for (let i = 0; i < n; i++) {
+        adjList[i] = [];
+    }
+    for (let i = 0; i < edges.length; i++) {
+        adjList[edges[i][0]].push(edges[i][1]);
+        adjList[edges[i][1]].push(edges[i][0]);
+    }
+    let queue = [source], visited = new Array(n).fill(false);
+    visited[source] = true;
+    let ind1 = 0, ind2 = 1;
+    while (ind1 < ind2) {
+        for (let el of adjList[queue[ind1]]) {
+            if (visited[el] === false) {
+                if (el === destination) {
+                    return true;
+                }
+                visited[el] = true;
+                queue.push(el);
+                ind2++;
+            }
+        }
+        ind1++;
+    }
+    return false;
+};
