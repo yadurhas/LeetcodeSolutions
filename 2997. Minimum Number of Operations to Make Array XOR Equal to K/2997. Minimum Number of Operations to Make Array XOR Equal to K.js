411464677e00bb99ee6d504b30e6fecf13dd274b
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-04-29 10:43:14 +0530

    Update 2997. Minimum Number of Operations to Make Array XOR Equal to K.js
    Solution to Daily Leetcode Challenge dated 29/04/2024

--- 2997. Minimum Number of Operations to Make Array XOR Equal to K/2997. Minimum Number of Operations to Make Array XOR Equal to K.js
+++ 2997. Minimum Number of Operations to Make Array XOR Equal to K/2997. Minimum Number of Operations to Make Array XOR Equal to K.js
@@ -1,1 +1,20 @@
-
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    let val = 0;
+    for (let num of nums) {
+        val ^= num;
+    }
+    let opCount = 0;
+    while (val !== 0 || k !== 0) {
+        if (val % 2 != k % 2) {
+            opCount++;
+        }
+        val = Math.floor(val / 2);
+        k = Math.floor(k / 2);
+    }
+    return opCount;
+};
