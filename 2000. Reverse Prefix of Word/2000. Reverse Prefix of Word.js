9ba55a29ed155414b058dcba05a018c3ca39239e
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-01 10:14:57 +0530

    Update 2000. Reverse Prefix of Word.js
    Solution to Daily Leetcode Challenge dated 01/05/2024

--- 2000. Reverse Prefix of Word/2000. Reverse Prefix of Word.js
+++ 2000. Reverse Prefix of Word/2000. Reverse Prefix of Word.js
@@ -1,1 +1,28 @@
-
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function(word, ch) {
+    function getIndex(word, ch) {
+        let index = 0;
+        while (index < word.length) {
+            if (word[index] === ch) {
+                return index;
+            }
+            index++;
+        }
+        return index;
+    }
+    let index = getIndex(word, ch);
+    if (index === word.length) {
+        return word;
+    } 
+    //console.log(index);
+    let res = word.slice(0, index + 1);
+    let t = res.split("");
+    //console.log(res, t);
+    t = t.reverse();
+    res = t.join("") + word.slice(index + 1);
+    return res;
+};
