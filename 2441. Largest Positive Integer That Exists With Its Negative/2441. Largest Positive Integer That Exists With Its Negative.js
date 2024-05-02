0cd50683d415eb4dbcc9b6a40e16f2d3bfb995a6
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-02 09:27:14 +0530

    Update 2441. Largest Positive Integer That Exists With Its Negative.js
    Solution to Daily Leetcode Challenge dated 02/05/2024

--- 2441. Largest Positive Integer That Exists With Its Negative/2441. Largest Positive Integer That Exists With Its Negative.js
+++ 2441. Largest Positive Integer That Exists With Its Negative/2441. Largest Positive Integer That Exists With Its Negative.js
@@ -1,1 +1,19 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxK = function(nums) {
+    let set1 = new Set();
+    for (let num of nums) {
+        set1.add(num);
+    }
+    let maxK = -1;
+    for (let value of set1.values()) {
+        if (value > maxK) {
+            if (set1.has(-1 * value)) {
+                maxK = value;
+            }
+        }
+    }
+    return maxK;
+};
