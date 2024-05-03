256b522948b6782a76ee7f8187c576c950de0a30
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-03 12:08:55 +0530

    Update 165. Compare Version Numbers.js
    Solution to Daily Leetcode Challenge dated 03/05/2024

--- 165. Compare Version Numbers/165. Compare Version Numbers.js
+++ 165. Compare Version Numbers/165. Compare Version Numbers.js
@@ -1,1 +1,59 @@
-
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function(version1, version2) {
+    function getRevisions(version) {
+        let arr = version.split('.');
+        let revisions = new Array(arr.length);
+        function getRevision(str) {
+            let num = 0;
+            for (let i = 0; i < str.length; i++) {
+                num = num * 10 + str.charCodeAt(i) - '0'.charCodeAt();
+            }
+            return num;
+        }
+        for (let i = 0; i < arr.length; i++) {
+            revisions[i] = getRevision(arr[i]);
+        }
+        return revisions;
+    }
+    function compareRevisions(revisions1, revisions2) {
+        let ind = 0, size1 = revisions1.length, size2 = revisions2.length; 
+        while (ind < size1 && ind < size2) {
+            if (revisions1[ind] < revisions2[ind]) {
+                return -1;
+            }
+            else if (revisions1[ind] > revisions2[ind]){
+                return 1;
+            }
+            ind++;
+        }
+        if (size1 === size2) {
+            return 0;
+        }
+        if (size1 < size2) {
+            while (ind < size2) {
+                if (revisions2[ind] > 0) {
+                    return -1;
+                }
+                ind++;
+            }
+            return 0;
+        }
+        if (size1 > size2) {
+            while (ind < size1) {
+                if (revisions1[ind] > 0) {
+                    return 1;
+                }
+                ind++;
+            }
+            return 0;
+        }
+    }
+    let revisions1, revisions2;
+    revisions1 = getRevisions(version1);
+    revisions2 = getRevisions(version2);
+    return compareRevisions(revisions1, revisions2);
+};
