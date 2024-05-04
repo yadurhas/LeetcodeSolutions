b5752ef82f9b02ed3cf45b25900def2851f1bc13
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-04 13:00:38 +0530

    Update 881. Boats to Save People.js
    Solution to Daily Leetcode Challenge dated 04/05/2024

--- 881. Boats to Save People/881. Boats to Save People.js
+++ 881. Boats to Save People/881. Boats to Save People.js
@@ -1,1 +1,21 @@
-
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    people.sort((a, b) => a - b);
+    let ind1 = 0, ind2 = people.length - 1;
+    let boatCount = 0;
+    while (ind1 < ind2) {
+        if (people[ind1] + people[ind2] <= limit) {
+            ind1++;
+        }
+        ind2--;
+        boatCount++;
+    }
+    if (ind1 === ind2) {
+        boatCount++;
+    }
+    return boatCount;
+};
