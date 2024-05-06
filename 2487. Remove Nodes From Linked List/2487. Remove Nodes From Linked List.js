9fe04111fe9172a4109afacccd610bb915ff14ec
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-06 13:02:10 +0530

    Update 2487. Remove Nodes From Linked List.js
    Solution to Daily Leetcode Challenge dated 06/05/2024

--- 2487. Remove Nodes From Linked List/2487. Remove Nodes From Linked List.js
+++ 2487. Remove Nodes From Linked List/2487. Remove Nodes From Linked List.js
@@ -1,1 +1,31 @@
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeNodes = function(head) {
+    let arr = [], node = head;
+    while (node !== null) {
+        if (arr.length > 0) {
+            while (arr[arr.length - 1].val < node.val) {
+                arr.pop();
+                if (arr.length === 0) {
+                    break;
+                }
+            }
+        }
+        arr.push(node);
+        node = node.next;
+    }
+    for (let i = 1; i < arr.length; i++) {
+        arr[i - 1].next = arr[i];
+    }
+    arr[arr.length - 1].next = null;
+    return arr[0];
+};
