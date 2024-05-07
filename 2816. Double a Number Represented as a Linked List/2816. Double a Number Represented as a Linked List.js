5b2befa0cb2c619f6556b25aa9cba61a3e5f4b9f
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-07 14:41:22 +0530

    Update 2816. Double a Number Represented as a Linked List.js
    Solution to Daily Leetcode Challenge dated 07/05/2024

--- 2816. Double a Number Represented as a Linked List/2816. Double a Number Represented as a Linked List.js
+++ 2816. Double a Number Represented as a Linked List/2816. Double a Number Represented as a Linked List.js
@@ -1,1 +1,32 @@
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var doubleIt = function(head) {
+    function doubleIt2(node) {
+        let t = new ListNode(0, node);
+        let val = node.val;
+        if (val * 2 >= 10) {
+            t.val = 1;
+        }
+        node.val = (node.val * 2) % 10;
+        if (node.next !== null) {
+            let nxt = doubleIt2(node.next);
+            node.val += nxt.val;
+            node.next = nxt.next;
+        }
+        return t;
+    }
+    let res = doubleIt2(head);
+    if (res.val === 0) {
+        res = res.next;
+    }
+    return res;
+};
