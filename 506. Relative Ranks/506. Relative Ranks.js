9b0ac0ef1109b8b6f5202f7e23c682ad7a625147
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-08 07:29:28 +0530

    Update 506. Relative Ranks.js
    Solution to Daily Leetcode Challenge dated 08/05/2024

--- 506. Relative Ranks/506. Relative Ranks.js
+++ 506. Relative Ranks/506. Relative Ranks.js
@@ -1,1 +1,32 @@
-
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function(score) {
+    let n = score.length;
+    let temp = new Array(n);
+    for (let i = 0; i < n; i++) {
+        temp[i] = [score[i], i];
+    }
+    function getRes(temp, n) {
+        res = new Array(n);
+        for (let i = 0; i < n; i++) {
+            res[temp[i][1]] = temp[i][0];
+        }
+        return res;
+    }
+    temp.sort((a, b) => b[0] - a[0]);
+    temp[0][0] = "Gold Medal";
+    if (n === 1) {
+        return getRes(temp, n);
+    }
+    temp[1][0] = "Silver Medal";
+    if (n === 2) {
+        return getRes(temp, n);
+    }
+    temp[2][0] = "Bronze Medal";
+    for (let i = 3; i < n; i++) {
+        temp[i][0] = i + 1 + "";
+    }
+    return getRes(temp, n);
+};
