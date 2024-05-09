933cd994d4271d19de5e4b7d202b311229f171db
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-09 12:09:26 +0530

    Update 3075. Maximize Happiness of Selected Children.js
    Solution to Daily Leetcode Challenge dated 09/05/2024

--- 3075. Maximize Happiness of Selected Children/3075. Maximize Happiness of Selected Children.js
+++ 3075. Maximize Happiness of Selected Children/3075. Maximize Happiness of Selected Children.js
@@ -1,1 +1,17 @@
-
+/**
+ * @param {number[]} happiness
+ * @param {number} k
+ * @return {number}
+ */
+var maximumHappinessSum = function(happiness, k) {
+    happiness.sort((a, b) => b - a);
+    let sum = 0, t;
+    for (let i = 0; i < k; i++) {
+        t = (happiness[i] - i);
+        if (t < 0) {
+            return sum;
+        }
+        sum += t;
+    }
+    return sum;
+};
