b57895dcbd2cd0c5b2ee2fab64f3d160009ae10d
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-12 14:41:50 +0530

    Update 2373. Largest Local Values in a Matrix.js
    Solution to Leetcode daily challenge dated 12/05/2024

--- 2373. Largest Local Values in a Matrix/2373. Largest Local Values in a Matrix.js
+++ 2373. Largest Local Values in a Matrix/2373. Largest Local Values in a Matrix.js
@@ -1,1 +1,16 @@
-
+/**
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+var largestLocal = function(grid) {
+    let n = grid.length;
+    n = n - 2;
+    let res = new Array(n);
+    for (let i = 0; i < n; i++) {
+        res[i] = new Array(n);
+        for (let j = 0; j < n; j++) {
+            res[i][j] = Math.max(grid[i][j], grid[i][j + 1], grid[i][j + 2], grid[i + 1][j], grid[i + 1][j + 1], grid[i + 1][j + 2], grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2]);
+        }
+    }
+    return res;
+};
