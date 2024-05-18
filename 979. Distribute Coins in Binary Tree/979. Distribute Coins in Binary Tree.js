c0402cc46bb6d7f76c2e574233bfa9ae6e4170ec
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-18 14:13:47 +0530

    Update 979. Distribute Coins in Binary Tree.js
    Solution to Daily Leetcode Challenge dated 18/05/2024

--- 979. Distribute Coins in Binary Tree/979. Distribute Coins in Binary Tree.js
+++ 979. Distribute Coins in Binary Tree/979. Distribute Coins in Binary Tree.js
@@ -1,1 +1,26 @@
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var distributeCoins = function(root) {
+    let minMoves = 0;
+    function traverse(node){
+        if (node !== null){
+            let val1 = traverse(node.left);
+            let val2 = traverse(node.right);
+            minMoves += Math.abs(val1) + Math.abs(val2);
+            return val1 + val2 + node.val - 1;
+        }
+        return 0;
+    }
+    traverse(root);
+    return minMoves;
+};
