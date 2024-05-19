5b760ae9694d1d061017cfc0a1cd28e34c914f5b
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-19 15:14:04 +0530

    Update 3068. Find the Maximum Sum of Node Values.js
    Solution to Daily Leetcode Challenge dated 19/05/2024

--- 3068. Find the Maximum Sum of Node Values/3068. Find the Maximum Sum of Node Values.js
+++ 3068. Find the Maximum Sum of Node Values/3068. Find the Maximum Sum of Node Values.js
@@ -1,1 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var maximumValueSum = function(nums, k, edges) {
+    let n = nums.length;
+    let arr = new Array(n);
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        arr[i] = [nums[i], (nums[i] ^ k) - nums[i]];
+        sum += nums[i];
+    }
+    /*
+        a, b
+        inorder (a[1] > b[1])
+        -ve
+        out of order (b[1] > a[1])
+        +ve
+        equals (a[1] = b[1])
+        0
+     */
 
+    arr.sort((a, b) => b[1] - a[1]);
+    for (let i = 0; i + 1 < n; i = i + 2) {
+        if (arr[i][1] + arr[i + 1][1] >= 0) {
+            sum += arr[i][1] + arr[i + 1][1];
+        } 
+    }
+    return sum;
+};
