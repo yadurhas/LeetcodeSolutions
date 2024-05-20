00ddf4a846615467051b4e4418019904aec5dc09
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-20 08:32:29 +0530

    Update 1863. Sum of All Subset XOR Totals.js
    Solution to Daily Leetcode Challenge dated 20/05/2024

--- 1863. Sum of All Subset XOR Totals/1863. Sum of All Subset XOR Totals.js
+++ 1863. Sum of All Subset XOR Totals/1863. Sum of All Subset XOR Totals.js
@@ -1,1 +1,19 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var subsetXORSum = function(nums) {
+    let sum = 0;
+    function traverse(ind, xorValue) {
+        if (ind < nums.length) {
+            traverse(ind + 1, xorValue);
+            traverse(ind + 1, xorValue ^ nums[ind]);
+        }
+        else {
+            sum += xorValue;
+        }
+        return;
+    }
+    traverse(0, 0);
+    return sum;
+};
