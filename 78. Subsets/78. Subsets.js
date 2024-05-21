2b9f63abc40ec02d2000317a83cdd09029d10b6f
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-21 10:30:29 +0530

    Update 78. Subsets.js
    Solution to Daily Leetcode Challenge dated 21/05/2024

--- 78. Subsets/78. Subsets.js
+++ 78. Subsets/78. Subsets.js
@@ -1,1 +1,19 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    function traverse(subsetsArr, nums, ind, arr) {
+        if (ind === nums.length) {
+            subsetsArr.push(Array.from(arr));
+            return;
+        }
+        traverse(subsetsArr, nums, ind + 1, arr);
+        arr.push(nums[ind]);
+        traverse(subsetsArr, nums, ind + 1, arr);
+        arr.pop();
+    }
+    let subsetsArr = [];
+    traverse(subsetsArr, nums, 0, []);
+    return subsetsArr;
+};
