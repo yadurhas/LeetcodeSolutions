40b9884909f79754019c6795cca3eea3da40ebdd
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-23 11:02:15 +0530

    Update 2597. The Number of Beautiful Subsets.js
    JS solution to Daily Leetcode Challenge dated 23/05/2024

--- 2597. The Number of Beautiful Subsets/2597. The Number of Beautiful Subsets.js
+++ 2597. The Number of Beautiful Subsets/2597. The Number of Beautiful Subsets.js
@@ -1,1 +1,30 @@
-
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var beautifulSubsets = function(nums, k) {
+    let count = 0;
+    function traverse(nums, ind, k, map) {
+        if (ind === nums.length) {
+            if (map.size !== 0) {
+                count++;
+            }
+            return;
+        }
+        traverse(nums, ind + 1, k, map);
+        if (!map.has(nums[ind] - k) && !map.has(nums[ind] + k)) {
+            if (!map.has(nums[ind])) {
+                map.set(nums[ind], 0);
+            }
+            map.set(nums[ind], map.get(nums[ind]) + 1);
+            traverse(nums, ind + 1, k, map);
+            map.set(nums[ind], map.get(nums[ind]) - 1);
+            if (map.get(nums[ind]) === 0) {
+                map.delete(nums[ind]);
+            }
+        }
+    }
+    traverse(nums, 0, k, new Map());
+    return count;
+};
