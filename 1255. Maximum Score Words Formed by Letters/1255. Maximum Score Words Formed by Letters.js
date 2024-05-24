330f7dbea5facb5a2c0d33861de4d6764c3796fd
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-24 12:43:21 +0530

    Update 1255. Maximum Score Words Formed by Letters.js
    JS backtracking solution to Daily Leetcode Challenge dated 24/05/2024

--- 1255. Maximum Score Words Formed by Letters/1255. Maximum Score Words Formed by Letters.js
+++ 1255. Maximum Score Words Formed by Letters/1255. Maximum Score Words Formed by Letters.js
@@ -1,1 +1,50 @@
-
+/**
+ * @param {string[]} words
+ * @param {character[]} letters
+ * @param {number[]} score
+ * @return {number}
+ */
+var maxScoreWords = function(words, letters, scores) {
+    let maxScore = Number.MIN_SAFE_INTEGER;
+    let remL = new Array(26).fill(0);
+    for (let i = 0; i < letters.length; i++) {
+        remL[letters[i].charCodeAt() - 'a'.charCodeAt()]++;
+    }
+    let t;
+    for (let i = 0; i < words.length; i++) {
+        t = new Array(26).fill(0);
+        for (let j = 0; j < words[i].length; j++) {
+            t[words[i].charCodeAt(j) - 'a'.charCodeAt()]++;
+        }
+        words[i] = t;
+    }
+    function possibleWord(word, letters) {
+        for (let i = 0; i < 26; i++) {
+            if (word[i] > letters[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    function traverse(words, ind, score, remL) {
+        if (ind === words.length) {
+            if (score > maxScore) {
+                maxScore = score;
+            }
+            return;
+        }
+        traverse(words, ind + 1, score, remL);
+        if (possibleWord(words[ind], remL)) {
+            for (let i = 0; i < 26; i++) {
+                remL[i] -= words[ind][i];
+                score += words[ind][i] * scores[i];
+            }
+            traverse(words, ind + 1, score, remL);
+            for (let i = 0; i < 26; i++) {
+                remL[i] += words[ind][i];
+            }
+        }
+    }
+    traverse(words, 0, 0, remL);
+    return maxScore;
+};
