39cf6158dfd7923b8a5a65989eca2485b1ee0837
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-25 11:20:10 +0530

    Update 140. Word Break II.js
    Backtracking JS solution for Daily Leetcode Challenge dated 25/05/2024

--- 140. Word Break II/140. Word Break II.js
+++ 140. Word Break II/140. Word Break II.js
@@ -1,1 +1,46 @@
-
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {string[]}
+ */
+var wordBreak = function(s, wordDict) {
+    let sentences = []
+    function isWord(letters) {
+        let t = letters.join("");
+        if (wordDict.includes(t)) {
+            return true;
+        } 
+        return false;
+    }
+    function getSentence(arr) {
+        let res;
+        res = arr[0].join("");
+        for (let i = 1; i < arr.length; i++) {
+            res += ' ' + arr[i].join("");
+        }
+        return res;
+    }
+    function traverse(s, arr, ind) {
+        if (ind === s.length) {
+            if (isWord(arr[arr.length - 1])) {
+                sentences.push(getSentence(arr));
+            }
+            return;
+        }
+        if (ind === 0) {
+            arr.push([s[ind]]);
+            traverse(s, arr, ind + 1);
+            return;
+        }
+        arr[arr.length - 1].push(s[ind]);
+        traverse(s, arr, ind + 1);
+        arr[arr.length - 1].pop();
+        if (isWord(arr[arr.length - 1])) {
+            arr.push([s[ind]]);
+            traverse(s, arr, ind + 1);
+            arr.pop();
+        }
+    }
+    traverse(s, [], 0);
+    return sentences;
+};
