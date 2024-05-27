940e04890027e96ca4f23b57b829311a5c6c46e3
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-27 12:14:20 +0530

    Update 1608. Special Array With X Elements Greater Than or Equal X.js
    Solution to Daily Leetcode Challenge dated 27/05/2024

--- 1608. Special Array With X Elements Greater Than or Equal X/1608. Special Array With X Elements Greater Than or Equal X.js
+++ 1608. Special Array With X Elements Greater Than or Equal X/1608. Special Array With X Elements Greater Than or Equal X.js
@@ -1,1 +1,18 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var specialArray = function(nums) {
+   nums.sort((a, b) => b - a);
+   if (nums[nums.length - 1] >= nums.length) {
+    return nums.length;
+   }
+   for (let i = 0; i < nums.length - 1; i++) {
+     if (nums[i] >= i + 1) {
+        if (nums[i + 1] < i + 1) {
+            return i + 1;
+        }
+     }
+   } 
+   return -1;
+};
