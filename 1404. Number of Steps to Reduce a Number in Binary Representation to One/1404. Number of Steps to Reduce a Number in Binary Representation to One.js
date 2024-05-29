25ed4e658381005dacea329ae131b7a0260dbc43
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-29 10:37:25 +0530

    Update 1404. Number of Steps to Reduce a Number in Binary Representation to One.js
    JS solution to Daily Leetcode Challenge dated 29/05/2024

--- 1404. Number of Steps to Reduce a Number in Binary Representation to One/1404. Number of Steps to Reduce a Number in Binary Representation to One.js
+++ 1404. Number of Steps to Reduce a Number in Binary Representation to One/1404. Number of Steps to Reduce a Number in Binary Representation to One.js
@@ -1,1 +1,28 @@
-
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSteps = function(s) {
+    let arr = s.split("");
+    console.log(arr);
+    let steps = 0;
+    function addOne(arr) {
+        for (let i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] === '0') {
+                arr[i] = '1';
+                return;
+            } 
+            arr[i] = '0';
+        }
+        arr.unshift('1');
+    }
+    while (arr.length != 1) {
+        steps++;
+        if (arr[arr.length - 1] === '0') {
+            arr.pop();
+            continue;
+        }
+        addOne(arr);
+    }
+    return steps;
+ };
