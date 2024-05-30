3409b2137bc603d8a1c890af10913dff958b801b
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-30 13:29:25 +0530

    Update 1442. Count Triplets That Can Form Two Arrays of Equal XOR.js
    JS solution to Daily Leetcode Challenge dated 30/05/2024

--- 1442. Count Triplets That Can Form Two Arrays of Equal XOR/1442. Count Triplets That Can Form Two Arrays of Equal XOR.js
+++ 1442. Count Triplets That Can Form Two Arrays of Equal XOR/1442. Count Triplets That Can Form Two Arrays of Equal XOR.js
@@ -1,1 +1,28 @@
-
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var countTriplets = function(arr) {
+    let count = 0;
+    for (let i = 1; i < arr.length; i++) {
+        arr[i] = arr[i - 1] ^ arr[i];
+    }
+    //console.log(arr);
+    let map1 = new Map();
+    map1.set(0, [-1]); //added to tackle the case where i is 0, need to write explanation
+    for (let i = 0; i < arr.length; i++){
+        if (!map1.has(arr[i])) {
+            map1.set(arr[i], []);
+        }
+        map1.get(arr[i]).push(i);
+    }
+    for (let key of map1.keys()) {
+        let a = map1.get(key);
+        for (let i = 0; i < a.length - 1; i++) {
+            for (let j = i + 1; j < a.length; j++) {
+                count += (a[j] - (a[i] + 1));
+            }
+        }
+    }
+    return count;
+};
