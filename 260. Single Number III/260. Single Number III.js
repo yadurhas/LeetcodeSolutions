761ec57ec53ff0330a57bebcf428a32751885b5d
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-31 15:03:32 +0530

    Update 260. Single Number III.js
    JS solution to daily Leetcode Challenge dated 31/05/2024

--- 260. Single Number III/260. Single Number III.js
+++ 260. Single Number III/260. Single Number III.js
@@ -1,1 +1,18 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function(nums) {
+    let set1 = new Set();
+    let set2 = new Set();
+    for (let num of nums) {
+        if (set1.has(num)) {
+            set2.delete(num);
+        }
+        else {
+            set1.add(num);
+            set2.add(num);
+        }
+    }
+    return Array.from(set2);
+};
