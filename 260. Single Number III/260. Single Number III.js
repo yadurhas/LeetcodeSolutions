3bd989804ddb2f95eeb8d8f6e6d898c2076953f7
yadurhas <30978478+yadurhas@users.noreply.github.com>
2024-05-31 15:16:18 +0530

    Update 260. Single Number III.js

--- 260. Single Number III/260. Single Number III.js
+++ 260. Single Number III/260. Single Number III.js
@@ -4,15 +4,13 @@
  */
 var singleNumber = function(nums) {
     let set1 = new Set();
-    let set2 = new Set();
     for (let num of nums) {
         if (set1.has(num)) {
-            set2.delete(num);
+            set1.delete(num);
         }
         else {
             set1.add(num);
-            set2.add(num);
         }
     }
-    return Array.from(set2);
+    return Array.from(set1);
 };
